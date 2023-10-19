98571ae2a23e2cf201f3766bf3b6c5283527d862
reetkaur1835 <144295525+reetkaur1835@users.noreply.github.com>
2023-10-18 22:13:40 -0400

    adding components to index.js

--- my-test-app/src/App.js
+++ my-test-app/src/App.js
@@ -1,13 +1,11 @@
 
 import './App.css';
-import AddProduct from './AddProduct'
-import SignUpUser from './SignUpUser'
+
 
 const App = () => {
   return (
     <div>
-      {<AddProduct />}
-      {<SignUpUser />}
+      
 
     </div>
   );
